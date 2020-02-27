bf3c938d2fcbd03959970926dca7537f626c1d53
Fabian <fabianlevinsky@gmail.com>
2020-02-27 20:44:22 -0300

    2

--- CarteleraCine/index.php
+++ CarteleraCine/index.php
@@ -10,6 +10,8 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        holaaaaaaaaaaaaaaaaaa
+        dosssssssssssssssss
         <?php
         // put your code here
         ?>
