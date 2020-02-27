5dac4b0d2fb8cb944d33f815b6a8b763aab732d0
Diego <diegozuro99@gmail.com>
2020-02-27 20:45:28 -0300

    skere

--- CarteleraCine/index.php
+++ CarteleraCine/index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         holaaaaaaaaaaaaaaaaaa
         dosssssssssssssssss
+        skere
         <?php
         // put your code here
         ?>
