a4e99254046f1a6757fde80d7b9352557ef6190d
Diego <diegozuro99@gmail.com>
2020-02-28 19:29:17 -0300

    Skere2

--- CarteleraCine/index.php
+++ CarteleraCine/index.php
@@ -1,12 +1,22 @@
-
 <?php
-//Incluyo la clase de Smarty
-require_once("smarty/libs/Smarty.class.php");
+
+require_once('smarty/libs/Smarty.class.php');
 
 //Creo un objeto template
 $miSmarty = new Smarty();
 
-//Despliego el template
+define('TEMPLATE_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/templates/');
+define('COMPILER_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/templates_c/');
+define('CONFIG_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/config/');
+define('CACHE_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/cache/');
+define(CONFIG_DIR, $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/templates/');
+
+$miSmarty->template_dir = TEMPLATE_DIR;
+$miSmarty->compile_dir = COMPILER_DIR;
+$miSmarty->config_dir = CONFIG_DIR;
+$miSmarty->cache_dir =CACHE_DIR;
+
 $miSmarty->display("index.tpl");
+
 
 ?>
