4beebea39098448846c0ffd9737961acd6c715c2
Fabian <fabianlevinsky@gmail.com>
2020-03-02 18:35:40 -0300

    funcion smarty

--- CarteleraCine/index.php
+++ CarteleraCine/index.php
@@ -9,7 +9,7 @@ define('TEMPLATE_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/templates/');
 define('COMPILER_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/templates_c/');
 define('CONFIG_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/config/');
 define('CACHE_DIR', $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/tmp/cache/');
-define(CONFIG_DIR, $_SERVER['DOCUMENT_ROOT'].'./CarteleraCine/templates/');
+
 
 $miSmarty->template_dir = TEMPLATE_DIR;
 $miSmarty->compile_dir = COMPILER_DIR;
